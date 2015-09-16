1c742943182daa0af285214197c3eb206237e075
Moses Jung <MosesJung86@gmail.com>
2015-09-16 16:13:29 -0700

    answers

--- app/controllers/answer.rb
+++ app/controllers/answer.rb
@@ -1,12 +1,15 @@
+#shows all answers
 get '/questions/:question_id/answers' do
   @answers = Answer.all
   erb :'/answers/index'
 end
 
+#new answer form
 get '/questions/:question_id/answers/new' do
   erb :'/answers/new'
 end
 
+#create new answer
 post '/questions/:question_id/answers' do
   answer = Answer.new(params[:answer])
   if answer.save
@@ -16,22 +19,26 @@ post '/questions/:question_id/answers' do
   end
 end
 
+#show specific answer
 get '/questions/:question_id/answers/:id' do
   @answer = Answer.find_by(id: params[:answer_id])
   erb :'/answers/show'
 end
 
+#answer edit form
 get '/questions/:question_id/answers/:id/edit' do
   @answer = Answer.find_by(id: params[:answer_id])
   erb :'/answers/edit'
 end
 
+#edits attributes
 put '/questions/:question_id/answers/:id' do
   @answer = Answer.find_by(id: params[:answer_id])
   @answer.update_attributes(params[:answer])
   redirect '/questions/#{:question_id}/answers/#{@answer.id}'
 end
 
+#deletes answer
 delete '/questions/:question_id/answers/:id' do
   @answer = Answer.find_by(id: params[:answer_id])
   @answer.destroy
