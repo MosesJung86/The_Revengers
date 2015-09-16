9e96ab37e068ff082e6cfd18dd2d3f656ce7e37c
alfredlam42 & Ben <pair+alfredlam42.benjamh@devbootcamp.com>
2015-09-16 11:50:35 -0700

    add timestamp to migration

--- db/migrate/20150916113526_create_questions.rb
+++ db/migrate/20150916113526_create_questions.rb
@@ -4,6 +4,7 @@ class CreateQuestions < ActiveRecord::Migration
       t.string  :title
       t.string  :description
       t.integer :vote_count, default: 0
+      t.timestamps
       t.references :author
   end
 end
